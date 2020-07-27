ca5362e448f520954875b499b9d352395bcd6cb9
Nick Coelius <nick@digit.co>
2020-07-27 12:15:49 -0700

    add slider for setting brush stroke size on drawing view

--- drawl/DrawingView.swift
+++ drawl/DrawingView.swift
@@ -2,21 +2,22 @@ import UIKit
 
 class DrawingView: UIView {
     var strokeColor: CGColor = UIColor.black.cgColor
+    var strokeSize: CGFloat = 5
     
-    private var lineArray: [[(point: CGPoint, strokeColor: CGColor)]] = [[(point: CGPoint, strokeColor: CGColor)]]()
+    private var lineArray: [[(point: CGPoint, strokeColor: CGColor, strokeSize: CGFloat)]] = [[(point: CGPoint, strokeColor: CGColor, strokeSize: CGFloat)]]()
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let firstPoint = touch.location(in: self)
 
-        lineArray.append([(point: CGPoint, strokeColor: CGColor)]())
-        lineArray[lineArray.count - 1].append((point: firstPoint, strokeColor: strokeColor))
+        lineArray.append([(point: CGPoint, strokeColor: CGColor, strokeSize: CGFloat)]())
+        lineArray[lineArray.count - 1].append((point: firstPoint, strokeColor: strokeColor, strokeSize: strokeSize))
     }
 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let currentPoint = touch.location(in: self)
-        lineArray[lineArray.count - 1].append((point: currentPoint, strokeColor: strokeColor))
+        lineArray[lineArray.count - 1].append((point: currentPoint, strokeColor: strokeColor, strokeSize: strokeSize))
         setNeedsDisplay()
     }
     
@@ -26,11 +27,12 @@ class DrawingView: UIView {
     }
 
     func draw(inContext context: CGContext) {
-        context.setLineWidth(5)
+        
         
         context.setLineCap(.round)
         for line in lineArray {
             guard let firstPoint = line.first else { continue }
+            context.setLineWidth(firstPoint.strokeSize)
             context.setStrokeColor(firstPoint.strokeColor)
             context.beginPath()
             context.move(to: firstPoint.point)
@@ -45,5 +47,9 @@ class DrawingView: UIView {
     //MARK: Helpers
     func setColor(newColor: CGColor) {
         self.strokeColor = newColor
+    }
+    
+    func setStrokeSize(newSize: CGFloat) {
+        self.strokeSize = newSize
     }
 }
--- drawl/SketchPadViewController.swift
+++ drawl/SketchPadViewController.swift
@@ -4,12 +4,17 @@ class SketchPadViewController: UIViewController {
     
     @IBOutlet weak var drawingView: DrawingView!
     @IBOutlet weak var dockView: UIView!
-
+    @IBOutlet weak var brushSizeSlider: UISlider!
+    @IBOutlet weak var brushSizeLabel: UILabel!
+    
+    private var lineArray: [[CGPoint]] = [[CGPoint]]()
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         styleView()
     }
+    
     @IBAction func colorSelected(_ sender: UIButton) {
         switch sender.backgroundColor?.cgColor {
         case UIColor.systemPurple.cgColor:
@@ -31,8 +36,11 @@ class SketchPadViewController: UIViewController {
         }
     }
     
-    private var lineArray: [[CGPoint]] = [[CGPoint]]()
-
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        brushSizeLabel.text = "\(Int(sender.value))"
+        drawingView.setStrokeSize(newSize: CGFloat(sender.value))
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let firstPoint = touch.location(in: drawingView)
@@ -49,8 +57,13 @@ class SketchPadViewController: UIViewController {
         drawingView.setNeedsDisplay()
     }
     
-    
     private func styleView() {
         dockView.addTopBorder(with: UIColor.black, andWidth: 1)
+        
+        brushSizeLabel.text = "\(5)"
+        
+        brushSizeSlider.isContinuous = true
+        brushSizeSlider.value = 5
+        drawingView.setStrokeSize(newSize: 5)
     }
 }
